450ae5bfaaddd13bef6ef27b85ec96d79d55d34f
Andy Stumpf <52007629+andystumpf@users.noreply.github.com>
2021-08-19 10:49:05 -0500

    updated function

--- execute.js
+++ execute.js
@@ -1,6 +1,13 @@
 //  create a Chrome Bookmark with the following URL
 //javascript:(function () {   const d = document;   const z = d.createElement('scr' + 'ipt');   const b = d.body;   const l = d.location;   try {     if (!b) throw 0;     z.setAttribute(       'src',       l.protocol + '//content.s3.amazonaws.com/execyte.js?t=' + new Date().getTime()     );     b.appendChild(z);   } catch (e) {     alert('Please wait until the page has loaded.');   } })();
 
+(function () {
+    const Team = prompt('Team');
+    if (!Team) return;
+    const featuresAdd = [000];
+    const featuresRemove = [001];
+
+
  featuresAdd.forEach((feature) =>
         fetch('https://website.com/Team/Add', {
             method: 'POST',
